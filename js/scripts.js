0dd6fcc168f29820c77732408b34790f961300f4
David Zhu and Hyung Lee <hyung@>
2018-08-13 11:31:36 -0700

    Finish html and JavaScript functions.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  var inputs = [];
+
+  $("#addList").click(function() {
+    var listInput = $("input#grocery").val();
+    if (listInput) {
+      inputs.push(listInput);
+      $("#initialList").text(inputs);
+    } else {
+      alert("Please enter valid item.");
+    }
+  })
+
+  $("#inputBox").submit(function(event) {
+    $("#inputBox").slideUp();
+    inputs.sort();
+    inputs.forEach(function(input) {
+      upperInput = input.toUpperCase();
+      $("ul").append("<li>" + upperInput + "</li>")
+    })
+
+    event.preventDefault();
+  })
+})
